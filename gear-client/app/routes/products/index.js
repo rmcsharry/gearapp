ce160660430b6b06c241dfce4c959be4fdde2044
Richard McSharry <Richard McSharry>
2016-08-23 19:19:37 +0200

    Wires up the product delete button

--- gear-client/app/routes/products/index.js
+++ gear-client/app/routes/products/index.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('product');
+    },
+
+    actions: {
+
+        deleteProduct(product) {
+            let confirmation = confirm(product.get('name').toString() + ' will be vaporized. Are you sure?');
+
+            if (confirmation) {
+                product.destroyRecord();
+            }
+        }
     }
 });
