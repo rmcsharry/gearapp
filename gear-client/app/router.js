ad374b7d19f88196f75366c8d53da35d1872251f
Richard McSharry <Richard McSharry>
2016-08-23 18:46:53 +0200

    Refactor category routes

--- gear-client/app/router.js
+++ gear-client/app/router.js
@@ -9,15 +9,14 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('categories', function() {
     this.route('new');
-    this.route('edit', { path: '/:category_id/edit'});
+    this.route('edit', { path: '/:category_id/edit' });
   });
-  this.route('category', { path: 'categories/:category_id'} );
 
   this.route('manufacturers');
 
   this.route('products', function() {
     this.route('new');
-    this.route('edit', { path: '/:product_id/edit'});
+    this.route('edit', { path: '/:product_id/edit' });
   });
 
   this.route('productAssets');
