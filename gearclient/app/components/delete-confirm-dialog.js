2c0548e20d41072c21d0593888912224de85ebfd
Richard McSharry <Richard McSharry>
2016-08-29 12:51:47 +0200

    Cleans up redundant files

--- gearclient/app/components/delete-confirm-dialog.js
+++ gearclient/app/components/delete-confirm-dialog.js
@@ -1,18 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    //objectToDelete: null,
-    
     actions: {
         submitConfirm() {
             // trigger action on calling parent
             this.get('onConfirm') (this.get('objectToDelete'));
         },
     },
-
-    // checkIsItemDirty() {
-    //     if (this.get('argument').get('isValid')) {
-    //         alert ('valid');
-    //     }
-    // }
 });
