553e50e1ea77aa8b86f65f38e3e37384f3db1ea8
Richard McSharry <Richard McSharry>
2016-08-30 04:33:23 +0200

    Adds highcharts pie to dashboard (test data)

--- gearclient/app/components/category-summary-chart.js
+++ gearclient/app/components/category-summary-chart.js
@@ -3,30 +3,56 @@ import defaultTheme from '../highcharts/themes/default-theme';
 
 export default Ember.Component.extend({
 
-  chartOptions: {
+  summaryData: [{
+    name: 'gear',
+    colorByPoint: true,
+    data: [
+        {y: 10, name: 'Test1'},
+        {y: 12, name: 'Test2'},
+        {y: 40, name: 'Test3'}
+        ]
+  }],
+
+  summaryOptions: {
     chart: {
-      type: 'bar'
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
     },
     title: {
-      text: 'Total weight of gear in each category'
+        text: 'Total weight of gear in each category'
     },
-    xAxis: {
-      categories: ['Test1', 'Test2', 'Test3']
+    tooltip: {
+        pointFormat: '<b>{point.percentage:.1f}%</b> of {series.name}'
     },
-    yAxis: {
-      title: {
-        text: 'Categories'
-      }
-    }
-  },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
 
-  chartData: [{
-    name: 'DummyA',
-    data: [1, 0, 4]
-  }, {
-    name: 'DummyB',
-    data: [5, 7, 3]
-  }],
+    legend: {
+        title: {
+            text: '<span style="font-size: 14px;">Categories</span><br/><span style="font-size: 10px; color: #666; font-weight: normal">(Click to hide)</span>',
+        },
+        backgroundColor: 'whitesmoke',
+        labelFormat: '{name} - {y:.2f}',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 4,
+        align: 'middle',
+        verticalAlign: 'top',
+        layout: 'vertical',
+        x: 0,
+        y: 100
+    }        
+  },
 
   theme: defaultTheme
 
--- gearclient/app/routes/dashboard.js
+++ gearclient/app/routes/dashboard.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    // this is for testing, normally we get the data from the store
+    model: function() {
+        return this.get('modelTestData');
+    },
+
+    setupController: function(controller, model) {
+        this._super(controller, model);
+        controller.set('mymodel', model);
+    },
+
+    modelTestData: [{
+        name: 'gear',
+        colorByPoint: true,
+        data: [
+            {y: 10, name: 'Test1'},
+            {y: 12, name: 'Test2'},
+            {y: 40, name: 'Test3'}
+            ]
+    }],
+ 
 });
